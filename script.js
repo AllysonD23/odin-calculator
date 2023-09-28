806fbc328a8fd82fb57475ecdff78723e63dc402
AllysonD23 <117053193+AllysonD23@users.noreply.github.com>
2023-09-27 22:08:38 -0500

    Added styling and setup files

---
+++ script.js
@@ -0,0 +1,25 @@
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(array) {
+    return a * b;
+  };
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+let firstNumber;
+
+let operator;
+
+let secondNumber;
+
+const operatorFunction = function(operator, firstNumber, secondNumber) {
+    return result = operator(firstNumber, secondNumber);
+}
