5c4b6ae2ab72eae50ceb0290aff9b1971d5561ae
AllysonD23 <117053193+AllysonD23@users.noreply.github.com>
2023-09-28 22:50:48 -0500

    Added final logic to calculator

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ const multiply = function(a, b) {
   };
 // Divide Function
 const divide = function(a, b) {
-    result = a / b;
+    if (secondNumber === 0) {
+        calculatorDisplay.innerHTML = "ERROR";
+    } else {
+        result = a / b;
+    }
 }
 
 // Setup variables for operator function
@@ -21,13 +25,12 @@ let operatorVariable;
 let secondNumber = 0;
 let firstNumberEntered;
 let operatorHit = false;
-let result;
+let result = 0;
 let equalsHit = false;
 
 // Operator Function
 const operatorFunction = function(operatorVariable, firstNumber, secondNumber) {
     operatorVariable(firstNumber, secondNumber);
-    calculatorDisplay.innerHTML = result;
 }
 
 // Check if operator has been hit function
@@ -52,66 +55,99 @@ const displayFunction = function(buttonPushed) {
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 7;
             equalsHit = false;
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();
             break;
         case "eight":
             displayValue += "8";
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 8;
             equalsHit = false;
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();
             break;
         case "nine":
             displayValue += "9";
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 9;
             equalsHit = false;
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();
             break;
         case "four":
             displayValue += "4";
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 4;     
-            equalsHit = false;       
+            equalsHit = false;   
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();    
             break;
         case "five":
             displayValue += "5";
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 5;
             equalsHit = false;
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();
             break;
         case "six":
             displayValue += "6";
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 6;
             equalsHit = false;
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();
             break;
         case "one":
             displayValue += "1";
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 1;
             equalsHit = false;
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();
             break;
         case "two":
             displayValue += "2";
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 2;
             equalsHit = false;
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();
             break;
         case "three":
             displayValue += "3";
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 3;
             equalsHit = false;
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();
             break;
         case "zero":
             displayValue += "0";
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 0;
             equalsHit = false;
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();
             break;    
         default:
             displayValue = "0";
             checkForOperatorHit();
             calculatorDisplay.innerHTML += 0;
             equalsHit = false;
+            checkForFirstNumber();
+            saveSecondNumber();
+            checkForThirdNumber();
             break;
     }
 };
@@ -143,10 +179,10 @@ const zeroButton = document.getElementById("zero").onclick = function(){
 
 // Create a function to check if first number has been entered
 const checkForFirstNumber = function() {
-    if (firstNumber != 0) {
-        return firstNumberEntered = true;
-    } else {
+    if (operatorHit === false) {
         return firstNumberEntered = false;
+    } else {
+        return firstNumberEntered = true;
     }
 }
 
@@ -156,6 +192,25 @@ const saveSecondNumber = function() {
         secondNumber = parseInt(displayValue);
     } else {
         firstNumber = parseInt(displayValue);
+    }
+}
+///////////// ADD check for if secondNumber is actually 0
+// Create a function to check if a third number has been entered
+const checkForThirdNumber = function() {
+    if (firstNumber != 0 && secondNumber != 0) {
+        operatorFunction(operatorVariable, firstNumber, secondNumber);
+        firstNumber = result;
+        secondNumber = 0;
+    }
+}
+///////////////////////////////////
+
+// Create a function to check if a result has been computed
+const checkForResult = function() {
+    if (result != 0) {
+        operatorFunction(operatorVariable, firstNumber, secondNumber);
+        firstNumber = result;
+        secondNumber = 0;
     }
 }
 
@@ -171,12 +226,13 @@ const clearButton = document.getElementById("clear-button").onclick = function()
 };
 
 const equalsButton = document.getElementById("equals-button").onclick = function(){
-    operatorFunction(operatorVariable, firstNumber, secondNumber);
-    firstNumber = 0;
+    //operatorFunction(operatorVariable, firstNumber, secondNumber);
+    calculatorDisplay.innerHTML = Math.round(result * 1000000) / 1000000;
+    firstNumber = result;
     secondNumber = 0;
     operatorVariable = 0;
     operatorHit = false;
-    result = 0;
+    //result = 0;
     equalsHit = true;
     displayValue = 0;
     };
@@ -184,29 +240,35 @@ const equalsButton = document.getElementById("equals-button").onclick = function
 const divideButton = document.getElementById("divide-button").onclick = function(){
     operatorVariable = divide;
     checkForFirstNumber();
-    saveSecondNumber();
+    //saveSecondNumber();
     displayValue = 0;
     operatorHit = true;
+    //checkForResult();
     };
 
 const multiplyButton = document.getElementById("multiply-button").onclick = function(){
     operatorVariable = multiply;
     checkForFirstNumber();
-    saveSecondNumber();
+    //saveSecondNumber();
     displayValue = 0;
-    operatorHit = true;};
+    operatorHit = true;
+    //checkForResult();
+};
 
 const subtractButton = document.getElementById("subtract-button").onclick = function(){
     operatorVariable = subtract;
     checkForFirstNumber();
-    saveSecondNumber();
+    //saveSecondNumber();
     displayValue = 0;
-    operatorHit = true;};
+    operatorHit = true;
+    //checkForResult();
+};
 
 const addButton = document.getElementById("add-button").onclick = function(){
     operatorVariable = add;
     checkForFirstNumber();
-    saveSecondNumber();
+    //saveSecondNumber();
     displayValue = 0;
     operatorHit = true;
-    };
+    //checkForResult();
+ };
